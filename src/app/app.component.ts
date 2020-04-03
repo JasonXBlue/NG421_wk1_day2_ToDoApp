7d43f0409d9ddd9cb26e939fcd6afee307dfcb77
JasonXBlue <JasonX.blue1@gmail.com>
2020-04-03 18:36:04 -0500

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'NG421wk1day1ToDoApp';
+  title = "Todos";
+  todoList: any[] = [];
+  todoTitle: string;
+  todoDate: Date = new Date();
+
+  ngOnInit() {
+    this.todoTitle = "";
+    this.todoList = [
+      // example of how to make an item in todo list
+      { title: "Install Angular CLI", isDone: false }
+    ];
+  }
+
+  // adds a todo to our list
+  addTodo(): void {
+    this.todoList.push({
+      title: this.todoTitle,
+      date: this.todoDate,
+      isDone: false
+    });
+
+    // resets our todoTitle variable to an empty string
+    this.todoTitle = "";
+  }
+
+  // a method to delete an item
+  deleteTodo(todo: any) {
+    const index = this.todoList.findIndex(todoItem => todoItem === todo);
+    this.todoList.splice(index, 1);
+  }
 }
